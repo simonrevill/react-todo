4cccfbf38606ce46928e44a3af9a139aa75d1e3f
Simon Revill <34922287+simonrevill@users.noreply.github.com>
2023-09-04 16:45:07 +0100

    initial commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/My Todo Application/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("renders a button with text 'Submit'", () => {
+  render(<App />);
+  const buttonElement = screen.getByText("Submit");
+  expect(buttonElement).toBeInTheDocument();
 });
